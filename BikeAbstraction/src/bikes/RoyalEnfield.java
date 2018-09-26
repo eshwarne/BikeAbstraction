6f71d8cd8a8a70d00c2dedfedfbc652dd4c3d3d6
eshwarne <northeasteditsofficial@gmail.com>
2018-09-26 08:43:25 +0530

    Royal Enfield Abstraction initiated

---
+++ BikeAbstraction/src/bikes/RoyalEnfield.java
@@ -0,0 +1,10 @@
+package bikes;
+
+public class RoyalEnfield extends Skeleton {
+	RoyalEnfield(String vm, String vo,String vt){
+		super(vm, vo, vt);
+	}
+	public void silencerSound() {
+		System.out.println("Doof.. Doof.. Doof")''
+	}
+}
--- BikeAbstraction/src/bikes/Skeleton.java
+++ BikeAbstraction/src/bikes/Skeleton.java
@@ -1,5 +1,16 @@
 package bikes;
 
-public class Skeleton {
-
+abstract class Skeleton {
+	public String vehicleNumber;
+	public String vehicleOwner;
+	public String vehicleType;
+	Skeleton(String vm, String vo,String vt){
+		this.vehicleNumber=vm;
+		this.vehicleOwner=vo;
+		this.vehicleType=vt;
+	}
+	abstract void silencerSound();
+	protected void changeVehicleOwner(String a) {
+		this.vehicleOwner=a;
+}
 }
